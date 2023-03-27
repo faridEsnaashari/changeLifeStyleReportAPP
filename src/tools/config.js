cc15b4021a543a4e26dc9ea506eb054862622ec1
faridEsnaashari <faride13578642@gmail.com>
2023-03-27 16:06:49 +0330

    local host api url added

--- src/tools/config.js
+++ src/tools/config.js
@@ -1,4 +1,5 @@
 const API_URL = "https://changelifestylereportapi-production.up.railway.app";
+//const API_URL = "http://localhost:3001";
 const APP_URL = "http://localhost:3000";
 
 export { API_URL, APP_URL };
